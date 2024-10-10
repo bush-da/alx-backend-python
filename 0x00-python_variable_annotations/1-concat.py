cf6d44ea221e990fc94b4324bf5b75385a6d7362
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-10 19:59:35 +0300

    concat two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that takes two string and concat them"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return two string concatnated"""
+    return str1 + str2
