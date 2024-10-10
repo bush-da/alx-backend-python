721e904a793abc1d35c73ee63b32f2cf76bd31cd
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-11 00:39:27 +0300

    add type annotations to the function

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Function that returns a value from the dictionary based on the key."""
+from typing import Mapping, Any, Union, TypeVar
+
+
+T = TypeVar('T')  # TypeVar for the default value type
+
+
+def safely_get_value(
+    dct: Mapping[Any, Any], key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """Returns the value from the dictionary for the given key,
+    or the default."""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
