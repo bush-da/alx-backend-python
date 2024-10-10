1c66eead4fbc18fa89423f330b8d580937e9b910
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-10 20:52:14 +0300

    sum list of float numbers

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Takes a list input_list of floats as argument and returns their sum"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Returns the sum floats number"""
+    return sum(input_list)
