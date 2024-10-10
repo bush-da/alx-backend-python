44c1f227e683e7bd2fe65e24a705d7ee1009fd51
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-10 22:39:41 +0300

    sum mixed numbers float and integer

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,6 +2,7 @@
 """Function which takes a list of integer and floats and returns their sum"""
 from typing import List, Union
 
+
 def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
     """Returns the sum of mixed float and integer numbers"""
     return sum(mxd_list)
