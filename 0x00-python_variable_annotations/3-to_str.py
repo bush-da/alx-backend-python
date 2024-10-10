e2d9e19e8889a8400bbf1f34212e1001f1152bb1
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-10 20:29:01 +0300

    return string representation of float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that takes float as argument and return string
+   representation of float"""
+
+
+def to_str(n: float) -> str:
+    """Return string representation of float number"""
+    return str(n)
