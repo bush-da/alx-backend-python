bd12bc8202e42137fb077bdff925d2becbe03a91
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-14 23:33:34 +0300

    fix pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> float:
     """run number of coroutine"""
     delays = []
