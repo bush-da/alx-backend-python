53028bfa11533774916ccbf591ba72e7c15cf91e
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-15 00:32:11 +0300

    fix return type

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """Execute multiple coroutines at the same time with async"""
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """run number of coroutine"""
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
