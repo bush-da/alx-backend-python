1e7dad76bee256eb4599a64c23fcfd2387d6b9ac
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-14 22:13:45 +0300

    sleep randomly

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,9 +5,9 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """generate random number b/n 0 and max_delay and
     sleep for number b/n max_delay and 0 and return time taken"""
-    max_delay = random.randint(0, max_delay) * random.random()
-    await asyncio.sleep(max_delay)
-    return max_delay
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
