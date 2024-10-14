fe8dc809be5d07fbdcd9608603a69c0274ff7a60
Daniel Tujuma <danielsevani12@gmail.com>
2024-10-15 01:19:43 +0300

    change async coroutine to regular function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,15 +7,16 @@ import asyncio
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Function that measure the total execution time for
     wait_n(n, max_delay) and
     Return:
           total_time / n
     """
+
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
 
-    total = end_time - start_time
-    return total / n
+    total_time = end_time - start_time
+    return total_time / n
